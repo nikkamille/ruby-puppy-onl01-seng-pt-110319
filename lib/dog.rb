362e12ee47243aec5ab03f628e85606dff6d84cc
Kamille Longalong <k.pagibigan@gmail.com>
2020-01-03 07:12:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
-# Add your code here
+class Dog 
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all 
+  end
+  
+  def self.print_all
+    puts @@all  
+  end
+  
+end
